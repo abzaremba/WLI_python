df6c79d73ade8b3db8fa66d5b546b8edb865c1f3
abzaremba <zaremba.a.a.b@gmail.com>
2025-06-01 12:23:25 +0100

    context/no context

--- hs9/hs9_fn.py
+++ hs9/hs9_fn.py
@@ -6,80 +6,10 @@ client = OpenAI(
     api_key=os.environ.get("OPENAI_API_KEY"),
 )
 
-
 # remember the model choices between completions and chat/completions
 # /v1/chat/completions: gpt-4, gpt-4-0314, gpt-4-32k, gpt-4-32k-0314, gpt-3.5-turbo, gpt-3.5-turbo-0301
 # /v1/completions:    text-davinci-003, text-davinci-002, text-curie-001, text-babbage-001, text-ada-001
 # remember about the different endpoints!!!
-
-# very short list
-protected_characteristics_default = [
-    'race', 'religion', 'sexual orientation', 'gender'
-]
-
-# # list that used to be our usual list
-# protected_characteristics = [
-#     'race', 'religion', 'ethnic origin', 'national origin', 'sex', 'disability', 'sexual orientation', 'gender identity'
-# ]
-
-
-
-# older HS definition
-# HS_definition = '"Hate speech" is speech that attacks a person or group on the basis of attributes such as: ' + ', '.join(protected_characteristics_default) +'.'
-
-HS_definition = 'Hate speech is defined as speech that attacks a person or group based on attributes such as ' + ', '.join(protected_characteristics_default) +'.'
-
-context_text = "";
-
-
-hs_examples_dict = [
-    {'example input':'You lost! Succer!',
-     'example output':['not hate speech',[], [1]]},
-    {'example input':'you hateful sack of flesh',
-     'example output':['not hate speech',[], [1]]},
-    {'example input':'“Pick one, kidd,” one nudged. Then, “Choose FA GO TT.”',
-     'example output':['hate speech',['sexual orientation'], [1]]},
-    {'example input':'“Kept being called a "Jew" for no reason.”; "A player on the other team was named Hitler.” “[One player] threatened to get antisemitic if I didnt talk. [They] talked about Jew tunnels.”',
-     'example output':['hate speech',['race','religion'], [1,1]]},
-    {'example input':'if money was grown on trees women would be dating monkeys oh wait never mind',
-     'example output':['hate speech',['sex'], [1]]},
-    {'example input':'well my dear lgbtq brothers and sisters i do not want to die at the hands of a muslim terrorist and as you are putting my life in danger with your politics i would like to suggest that you all go fuck yourselves',
-     'example output':['hate speech',['gender identity', 'sexual orientation', 'religion'],  [0.97,0.97,1]]}
-]
-
-
-hs_examples_str = '\n'
-for ind in range(len(hs_examples_dict)):
-  hs_examples_str += "\n" + str(hs_examples_dict[ind])[1:-1]
-
-
-
-chain_of_thought_dict = [
-    {'example input':'You lost! Succer!',
-     'example output':['not hate speech',[], [1]],
-     "explanation":"This statement expresses frustration and is an insult but does not target any group or individual based on identity. So it is not hate speech."},
-    {'example input':'you hateful sack of flesh',
-     'example output':['not hate speech',[], [1]],
-     "explanation":"The statement is a personal insult but does not target any specific group or individual based on identity. So it is not hate speech."},
-    {'example input':'“Pick one, kidd,” one nudged. Then, “Choose FA GO TT.”',
-     'example output':['hate speech',['sexual orientation'], [1]],
-     "explanation":"The use of the slur 'FA GO TT' is a direct attack on sexual orientation, intended to demean or insult. So it is hate speech."},
-    {'example input':'“Kept being called a "Jew" for no reason.”; "A player on the other team was named Hitler.” “[One player] threatened to get antisemitic if I didnt talk. [They] talked about Jew tunnels.”',
-     'example output':['hate speech',['race','religion'], [1,1]],
-     "explanation":"The use of antisemitic language, association with Hitler, and threats based on Jewish identity constitute hate speech targeting race and religion."},
-    {'example input':'if money was grown on trees women would be dating monkeys oh wait never mind',
-     'example output':['hate speech',['sex'], [1]],
-     "explanation":"This statement uses stereotypes to demean women by comparing them to animals, which is hate speech based on sex."},
-    {'example input':'well my dear lgbtq brothers and sisters i do not want to die at the hands of a muslim terrorist and as you are putting my life in danger with your politics i would like to suggest that you all go fuck yourselves',
-     'example output':['hate speech',['gender identity', 'sexual orientation', 'religion'],  [0.97,0.97,1]],
-     "explanation":"This statement combines hostility toward the LGBTQ community with derogatory references to religion, specifically Muslims, and assigns blame. It targets multiple identities and is hate speech."}
-]
-
-
-chain_ot_str = '\n'
-for ind in range(len(chain_of_thought_dict)):
-  chain_ot_str += "\n" + str(chain_of_thought_dict[ind])[1:-1]
-
 
 def classify_hs(message:str, protected_characteristics_str:str, HS_definition:str, examples:str, chain_ot:str, verbose=False, 
                 community_context="", languages = "", geography = "", safeguarding_focus = "", model="gpt-3.5-turbo"):
@@ -149,3 +79,16 @@ def classify_hs(message:str, protected_characteristics_str:str, HS_definition:st
     )
 
     return response
+
+def prepare_config(df, ii = 0):
+  # returns config variable with relevant values about community/context
+  # reads in a dataframe and a row
+  config_values = {
+      'community_type':df['Community type'][ii],
+      'community_context':df['Context'][ii],
+      'geography':df['Geography'][ii],
+      'protected_characteristics_str':df['Community values'][ii],
+      'languages':df['Language Used'][ii],
+      'safeguarding_focus':df['Safeguarding Focus'][ii]
+  }
+  return config_values
---
+++ hs9/hs9_script_cd.py
@@ -0,0 +1,178 @@
+from openai import OpenAI
+import os
+from hs9_fn import classify_hs, prepare_config
+from pprint import pprint
+
+client = OpenAI(api_key = os.environ.get("OPENAI_API_KEY"))
+
+import pandas as pd
+
+############################################################
+# read information about community context
+# and messages to analyse
+ 
+contexts_file_name   = "hs9/data/Demo_context_impact - context.csv"
+messages_file_name   = "hs9/data/Demo_context_impact - message.csv"
+
+df_context = pd.read_csv(contexts_file_name)
+messages = pd.read_csv(messages_file_name, header = None)
+messages.rename(columns={0: 'message'}, inplace=True)
+
+
+# # do you want to choose a community context and message now? or later in a loop?
+# row_context = 0
+# row_message = 0
+# config_values = prepare_config(df_context, row_context)
+# message = messages['message'][row_message]
+
+# pprint(config_values)
+# print(messages)
+
+############################################################
+# PROTECTED CHARACTERISTICS!
+# OR CATEGORIES 
+
+# very short list
+protected_characteristics_default = [
+    'race', 'religion', 'sexual orientation', 'gender' 
+#   'race', 'religion', 'ethnic origin', 'national origin', 'sex', 'disability', 'sexual orientation', 'gender identity'
+]
+
+# DEFINITION OF HATE SPEECH:1
+HS_definition = 'Hate speech is defined as speech that attacks a person or group based on attributes such as ' + ', '.join(protected_characteristics_default) +'.'
+############################################################
+
+
+############################################################
+# EXAMPLES!!!
+hs_examples_dict = [
+    {'example input':'You lost! Succer!',
+     'example output':['not hate speech',[], [1]]},
+    {'example input':'you hateful sack of flesh',
+     'example output':['not hate speech',[], [1]]},
+    {'example input':'“Pick one, kidd,” one nudged. Then, “Choose FA GO TT.”',
+     'example output':['hate speech',['sexual orientation'], [1]]},
+    {'example input':'“Kept being called a "Jew" for no reason.”; "A player on the other team was named Hitler.” “[One player] threatened to get antisemitic if I didnt talk. [They] talked about Jew tunnels.”',
+     'example output':['hate speech',['race','religion'], [1,1]]},
+    {'example input':'if money was grown on trees women would be dating monkeys oh wait never mind',
+     'example output':['hate speech',['sex'], [1]]},
+    {'example input':'well my dear lgbtq brothers and sisters i do not want to die at the hands of a muslim terrorist and as you are putting my life in danger with your politics i would like to suggest that you all go fuck yourselves',
+     'example output':['hate speech',['gender identity', 'sexual orientation', 'religion'],  [0.97,0.97,1]]}
+]
+
+
+hs_examples_str = '\n'
+for ind in range(len(hs_examples_dict)):
+  hs_examples_str += "\n" + str(hs_examples_dict[ind])[1:-1]
+############################################################
+
+############################################################
+# CHAIN OF THOUGHT EXAMPLES!!!
+chain_of_thought_dict = [
+    {'example input':'You lost! Succer!',
+     'example output':['not hate speech',[], [1]],
+     "explanation":"This statement expresses frustration and is an insult but does not target any group or individual based on identity. So it is not hate speech."},
+    {'example input':'you hateful sack of flesh',
+     'example output':['not hate speech',[], [1]],
+     "explanation":"The statement is a personal insult but does not target any specific group or individual based on identity. So it is not hate speech."},
+    {'example input':'“Pick one, kidd,” one nudged. Then, “Choose FA GO TT.”',
+     'example output':['hate speech',['sexual orientation'], [1]],
+     "explanation":"The use of the slur 'FA GO TT' is a direct attack on sexual orientation, intended to demean or insult. So it is hate speech."},
+    {'example input':'“Kept being called a "Jew" for no reason.”; "A player on the other team was named Hitler.” “[One player] threatened to get antisemitic if I didnt talk. [They] talked about Jew tunnels.”',
+     'example output':['hate speech',['race','religion'], [1,1]],
+     "explanation":"The use of antisemitic language, association with Hitler, and threats based on Jewish identity constitute hate speech targeting race and religion."},
+    {'example input':'if money was grown on trees women would be dating monkeys oh wait never mind',
+     'example output':['hate speech',['sex'], [1]],
+     "explanation":"This statement uses stereotypes to demean women by comparing them to animals, which is hate speech based on sex."},
+    {'example input':'well my dear lgbtq brothers and sisters i do not want to die at the hands of a muslim terrorist and as you are putting my life in danger with your politics i would like to suggest that you all go fuck yourselves',
+     'example output':['hate speech',['gender identity', 'sexual orientation', 'religion'],  [0.97,0.97,1]],
+     "explanation":"This statement combines hostility toward the LGBTQ community with derogatory references to religion, specifically Muslims, and assigns blame. It targets multiple identities and is hate speech."}
+]
+
+chain_ot_str = '\n'
+for ind in range(len(chain_of_thought_dict)):
+  chain_ot_str += "\n" + str(chain_of_thought_dict[ind])[1:-1]
+############################################################
+
+
+# verbose?
+verbose_switch = False
+
+# read community context and message now in a loop?
+row_context = 0
+row_message = 0
+config_values = prepare_config(df_context, row_context)
+message = messages['message'][row_message]
+
+# pprint(config_values)
+print(messages)
+
+# no request for input, just use the context and message read from csv files
+for row_context in range(1): #range(df_context.shape[0]):
+    print(row_context)
+    config_values = prepare_config(df_context, row_context)
+    pprint(config_values)
+
+    print(f"{'#'*20} Context provided: {' '*20} {'#'*20} ")
+    print(f"\n{'#'*5} community_context:\n{config_values['community_context']}")
+    print(f"\n{'#'*5} languages:\n{config_values['languages']}")
+    print(f"\n{'#'*5} geography:\n{config_values['geography']}")
+    print(f"\n{'#'*5} protected_characteristics_str:\n{config_values['protected_characteristics_str']}")
+    print(f"\n{'#'*5} safeguarding_focus:\n{config_values['safeguarding_focus']}")
+    for row_message in range(messages.shape[0]):     
+          print(row_message)
+          message = messages['message'][row_message]
+          print(f"{'#'*20} Context message:\n")
+          print(message)
+          print(f"{'#'*80} ")    
+          
+          # with context 
+          response = classify_hs(
+                 message = message, 
+                 protected_characteristics_str = config_values['protected_characteristics_str'], 
+                 HS_definition=HS_definition, 
+                 examples=hs_examples_str,
+                 chain_ot=chain_ot_str,
+                 verbose = verbose_switch,
+                 community_context=config_values['community_context'], 
+                 languages = config_values['languages'], 
+                 geography = config_values['geography'], 
+                 safeguarding_focus = config_values['safeguarding_focus']
+                 )
+          print(response.choices[0].message.content)
+
+          # without context 
+          print(f"{'#'*80} ")
+          print(f"{'#'*20} Without context!!!")
+          response = classify_hs(
+                 message = message, 
+                 protected_characteristics_str = '', 
+                 HS_definition=HS_definition, 
+                 examples=hs_examples_str,
+                 chain_ot=chain_ot_str,
+                 verbose = verbose_switch,
+                 community_context='', 
+                 languages = '', 
+                 geography = '', 
+                 safeguarding_focus = ''
+                 )
+          print(response.choices[0].message.content)
+
+          # without context, examples or chain of thought
+          print(f"{'#'*80} ")
+          print(f"{'#'*20} Without context, examples or chain of thought!!!")
+          response = classify_hs(
+                 message = message, 
+                 protected_characteristics_str = '', 
+                 HS_definition=HS_definition, 
+                 examples=[],
+                 chain_ot=[],
+                 verbose = verbose_switch,
+                 community_context='', 
+                 languages = '', 
+                 geography = '', 
+                 safeguarding_focus = ''
+                 )
+          print(response.choices[0].message.content)
+          
+    
